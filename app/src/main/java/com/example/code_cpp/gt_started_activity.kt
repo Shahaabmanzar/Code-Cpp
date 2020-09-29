0473e95d438fc2abac52f26831c826fca855feb7
atif9 <mohammadatif92080@gmail.com>
2020-09-30 01:04:12 +0530

    new ui

---
+++ app/src/main/java/com/example/code_cpp/gt_started_activity.kt
@@ -0,0 +1,12 @@
+package com.example.code_cpp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class gt_started_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gt_started_activity)
+    }
+}
--- app/src/main/java/com/example/code_cpp/main_menu.kt
+++ app/src/main/java/com/example/code_cpp/main_menu.kt
@@ -4,12 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import kotlinx.android.synthetic.main.activity_main_menu.*
 
 class main_menu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+
+        getting_started_btn.setOnClickListener {
+            val intent=Intent(this,gt_started_activity::class.java)
+            startActivity(intent)
+        }
 
     }
     }
