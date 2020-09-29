4fc160d0a3c719d5baf555e01bbd068f3f53f1c6
atif9 <mohammadatif92080@gmail.com>
2020-09-29 19:31:09 +0530

    main page ui

--- app/src/main/java/com/example/code_cpp/MainActivity.kt
+++ app/src/main/java/com/example/code_cpp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.code_cpp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed(
+            {
+                val int= Intent(this,main_menu::class.java)
+                startActivity(int)
+            },2000
+        )
     }
 }
--- app/src/main/java/com/example/code_cpp/main_menu.kt
+++ app/src/main/java/com/example/code_cpp/main_menu.kt
@@ -1,12 +1,16 @@
 package com.example.code_cpp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class main_menu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
     }
-}
+    }
+
