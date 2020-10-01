cd43ab7d519250c36306c4ae6f0934e38c28a227
atif9 <mohammadatif92080@gmail.com>
2020-10-01 20:34:05 +0530

    commit 2

--- app/src/main/java/com/example/code_cpp/gt_started_activity.kt
+++ app/src/main/java/com/example/code_cpp/gt_started_activity.kt
@@ -1,12 +1,20 @@
 package com.example.code_cpp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_gt_started_activity.*
 
 class gt_started_activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gt_started_activity)
+
+        whycpp_btn.setOnClickListener {
+
+            val intent=Intent(this,whycpp::class.java)
+            startActivity(intent)
+        }
     }
 }
