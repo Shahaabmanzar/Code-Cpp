8a37cf06318613f18edb26bcc5a36ee086a2d67d
atif9 <mohammadatif92080@gmail.com>
2020-10-08 02:45:45 +0530

    sharedprefrences on ide

--- app/src/main/java/com/example/code_cpp/Editor.kt
+++ app/src/main/java/com/example/code_cpp/Editor.kt
@@ -12,9 +12,19 @@ class Editor : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor)
 
+        val sharedevent=getSharedPreferences("ide",0)
+
+        var text=sharedevent.getString("idestring","//write code here").toString()
+        ide_text.setText(text)
         run_btn.setOnClickListener{
             Toast.makeText(this,"You click float btn",Toast.LENGTH_SHORT).show()
+
+            val editor=sharedevent.edit()
+            editor.putString("idestring",ide_text.text.toString())
+            editor.apply()
+            editor.commit()
         }
+
 
     }
 }
