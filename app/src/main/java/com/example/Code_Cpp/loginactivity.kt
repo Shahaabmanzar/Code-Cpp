7a39fbce23d72fb60c290d585a6c603fe3fcf1d8
atif9 <mohammadatif92080@gmail.com>
2020-10-31 23:26:43 +0530

    login working

--- app/src/main/java/com/example/Code_Cpp/loginactivity.kt
+++ app/src/main/java/com/example/Code_Cpp/loginactivity.kt
@@ -3,19 +3,22 @@ package com.example.Code_Cpp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_loginactivity.*
 
 class loginactivity : AppCompatActivity() {
-
+    private lateinit var auth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loginactivity)
         supportActionBar?.hide()
 
-
+        auth=Firebase.auth
         login_btn.setOnClickListener {
-            val intent=Intent(this,main_menu::class.java)
-            startActivity(intent)
+            login()
         }
 
         signupbtn.setOnClickListener {
@@ -23,4 +26,34 @@ class loginactivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    fun login()
+    {
+
+        if(emailtxt.text.toString().isEmpty())
+        {
+            Toast.makeText(this,"Enter Email",Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(passtext.text.toString().isEmpty())
+        {
+            Toast.makeText(this,"Enter Password",Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(emailtxt.text.toString(), passtext.text.toString())
+    .addOnCompleteListener(this) { task ->
+        if (task.isSuccessful) {
+            val intent = Intent(this, main_menu::class.java)
+            val user=Firebase.auth.currentUser
+            Toast.makeText(this,"Welcome ${user?.displayName}",Toast.LENGTH_LONG).show()
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Invalid Login", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
+
+
 }
